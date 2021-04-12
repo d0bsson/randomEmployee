00d22048b66fba36d8d8747af8565815a93b6ff0
David <ber.dev@yandex.ru>
2021-04-12 23:50:01 +0300

    Initial Commit

---
+++ randomEmployee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomEmployee
+//
+//  Created by Дэвид Бердников on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
